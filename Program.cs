348c4d3a53f24fa38055b5e37c3a0ddd30623681
rewerze <gamermod801@gmail.com>
2024-10-07 11:33:32 +0200

    added files

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using Sprint_Verseny;
+using System.Text;
+
+
+List<Versenyzo> versenyzo = [];
+
+StreamReader sr = new StreamReader("forras.txt", Encoding.UTF8);
+while (!sr.EndOfStream) versenyzo.Add(new(sr.ReadLine()));
+sr.Close();
+
+
+
+// írd ki a konzolra, hogy hány versenyző fejezte be a versenyt
+Console.WriteLine("Darab feladat:");
+Console.WriteLine($"{versenyzo.Count()} db versenyző fejezte be a versenyt");
+
+// versenyzők száma 'elit' kategóriában
+var elitSzam = versenyzo
+    .FindAll(v => v.Kategoria == "elit junior")
+    .Count();
+Console.WriteLine($"Elitek száma: {elitSzam}");
+
+// női versenyzők átlagéletkora
+var nokAtlagEletkora = versenyzo
+    .FindAll(v => v.Nem == "n")
+    .Average(v => DateTime.Now.Year - v.SzulEv);
+
+Console.WriteLine($"Nők átlag életkora: {nokAtlagEletkora}");
+
+
+// a versenyzők kerékpározással töltött összideje órában. 2tj-ig kerekítve
+
+
+// átlagos úszási idő elit junior kategóriában
+var atlagUszIdo = versenyzo
+    .FindAll(v => v.Kategoria == "elit junior")
+    .Average(v => v.Uszas.Minute);
+
+Console.WriteLine($"Az elit junior versenyzők átlag úszási ideje: {atlagUszIdo} perc");
+
+
+// férfi győztes (legrövidebb összidő)
+
+
+
+// korkategóriánként a versenyt befejezők száma
+var kategoriankent = versenyzo
+    .GroupBy(v => v.Kategoria);
+foreach (var group in kategoriankent
+    .OrderBy(g => g.Key))
+{
+    Console.WriteLine($"{group.Key}");
+    foreach (var befejezo in group)
+    {
+        Console.WriteLine($"\t[{versenyzo.IndexOf(befejezo):00}] {befejezo}");
+    }
+}
+
+
+// korkategóriánként az átlag depóban töltött idő
---
+++ Versenyzo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint_Verseny
+{
+    internal class Versenyzo
+    {
+        public string Nev { get; set; }
+        public int SzulEv { get; set; }
+        public int Rajtszam { get; set; }
+        public string Nem { get; set; }
+        public string Kategoria { get; set; }
+        public DateTime Uszas { get; set; }
+        public DateTime DepoEgy { get; set; }
+        public DateTime Kerekpar { get; set; }
+        public DateTime DepoKetto { get; set; }
+        public DateTime Futas { get; set; }
+
+        public Versenyzo(string sor) {
+            var darab = sor.Split(';');
+            Nev = darab[0];
+            SzulEv = int.Parse(darab[1]);
+            Rajtszam = int.Parse(darab[2]);
+            Nem = darab[3];
+            Kategoria = darab[4];
+            Uszas = DateTime.Parse(darab[5]);
+            DepoEgy = DateTime.Parse(darab[6]);
+            Kerekpar = DateTime.Parse(darab[7]);
+            DepoKetto = DateTime.Parse(darab[8]);
+            Futas = DateTime.Parse(darab[9]);
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Nev}:\n\tKor: {DateTime.Now.Year - SzulEv}\n\tRajtszam: {Rajtszam}\n\tNem: {Nem}\n\tKategória: {Kategoria}";
+        }
+    }
+}
